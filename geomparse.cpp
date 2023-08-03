ad0427ace108e9396d107fa0ac06ddb2266f217b
blackstar <tribalrage@gmail.com>
2023-08-03 21:31:46 +0200

    nibble D mistake

--- geomparse.cpp
+++ geomparse.cpp
@@ -402,7 +402,7 @@ NibbleAction nibble_actions[NUM_NIBBLES] =
     NibbleAction(  0,  0,  0,     0,  0,  0,     0),   // A
     NibbleAction(  0,  0,  0,     0,  0,  0,     0),   // B
     NibbleAction(  0,  0,  0,     0,  0,  0,     0),   // C
-    NibbleAction(  0,  1,  2,     3,  1,  3,     4),   // D
+    NibbleAction(  0,  1,  2,     2,  1,  3,     4),   // D
     NibbleAction(  0,  1,  2,     1,  0,  3,     3),   // E
     NibbleAction(  0,  1,  2,     3,  4,  5,     6),   // F
 };
